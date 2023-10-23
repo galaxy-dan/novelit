380c1079151e31f748a7bf7af088c64f8b910d12
sjyoon <sjyoon4755@gmail.com>
2023-10-23 15:31:39 +0900

    create server project

---
+++ server/src/test/java/com/galaxy/novelit/NovelitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.novelit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
