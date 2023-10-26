f78a06d648dc644d140bb4a308388836ca5f6d4f
jhp336 <jhp336@naver.com>
2023-10-26 12:43:46 +0900

    [S09P31A602-257] feat: 파일 조회 응답을 위한 DTO 추가

---
+++ server/src/main/java/com/galaxy/novelit/directory/dto/response/FileResDTO.java
@@ -0,0 +1,12 @@
+package com.galaxy.novelit.directory.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class FileResDTO {
+	private String content;
+}
