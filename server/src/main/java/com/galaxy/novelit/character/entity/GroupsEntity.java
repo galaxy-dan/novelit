c796c7f0310e634f0c24ecdc2ba8f30ac91311d2
sjyoon <sjyoon4755@gmail.com>
2023-10-27 13:39:11 +0900

    feat: create groups entity

--- server/src/main/java/com/galaxy/novelit/character/entity/GroupsEntity.java
+++ server/src/main/java/com/galaxy/novelit/character/entity/GroupsEntity.java
@@ -9,25 +9,25 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-//@Getter
-//@AllArgsConstructor
-//@Builder
-//@NoArgsConstructor
-//@Entity(name = "groups")
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Entity(name = "groups")
 public class GroupsEntity {
-//    @Id
-//    @Column(name = "group_id")
-//    private Long groupId;
-//    @Id
-//    @Column(name = "workspace_uuid")
-//    private UUID workspaceUuid;
-//    @Column(name = "group_id")
-//    private UUID groupUuid;
-//    @Column(name = "group_name")
-//    private String groupName;
-//    @Column(name = "parent_group")
-//    private UUID parentGroup;
-//    @Column(name = "is_deleted")
-//    private boolean isDeleted;
-//
+    @Id
+    @Column(name = "group_id")
+    private Long groupId;
+    @Id
+    @Column(name = "workspace_uuid")
+    private UUID workspaceUuid;
+    @Column(name = "group_id")
+    private UUID groupUuid;
+    @Column(name = "group_name")
+    private String groupName;
+    @Column(name = "parent_group")
+    private UUID parentGroup;
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+
 }
