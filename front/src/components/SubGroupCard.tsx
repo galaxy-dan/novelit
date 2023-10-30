572d20def9f645d5ce47b461c4cbd9b94f6dd2a0
ssh102525 <ssh102525@naver.com>
2023-10-30 17:44:48 +0900

    feat: 서브 그룹 카드 컴포넌트 추가

---
+++ front/src/components/SubGroupCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function SubGroupCard() {
+  return (
+    <div
+      className="flex border-2 rounded-md w-72 h-40 px-3 items-center shadow-lg mt-8 cursor-pointer"
+      onClick={() => {}}
+    >
+      <div className="ml-4 w-36 my-auto">
+        <p className="font-extrabold text-2xl">서브그룹</p>
+      </div>
+    </div>
+  );
+}
