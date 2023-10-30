0fff68f8f5aa27027b52e75ea5698bda0cda3b60
jhp336 <jhp336@naver.com>
2023-10-30 12:55:22 +0900

    fix: cors 에러 설정 추가

--- server/src/main/java/com/galaxy/novelit/config/WebConfig.java
+++ server/src/main/java/com/galaxy/novelit/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedMethods("*")
+            .allowedOrigins("*")
             .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
             .allowedHeaders("*");
     }
