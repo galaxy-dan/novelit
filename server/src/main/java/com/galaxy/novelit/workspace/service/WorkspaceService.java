1e7339434971f67a6fc44344b5d623eb2d7efd38
chemwang13 <chemwang13@naver.com>
2023-10-30 15:52:43 +0900

    feat: get workspaces feature and delete method modified

--- server/src/main/java/com/galaxy/novelit/workspace/service/WorkspaceService.java
+++ server/src/main/java/com/galaxy/novelit/workspace/service/WorkspaceService.java
@@ -1,8 +1,12 @@
 package com.galaxy.novelit.workspace.service;
 
+import java.util.List;
+
 public interface WorkspaceService {
     void createWorkspace(String workSpaceUUID, String user_uuid, String title);
     void modifyWorkspaceName(String workSpaceUUID, String title);
 
     void deleteWorkspace(String workSpaceUUID);
+
+    List<?> getWorkspaces(String testUUID);
 }
