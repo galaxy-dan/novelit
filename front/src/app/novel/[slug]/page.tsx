f071b3d75fd47ae19fc07ea69d66822934662bc4
JeonJungyu <wjswnsrb93@naver.com>
2023-10-31 14:07:15 +0900

    feat: 소설 리스트 컴포넌트 추가

--- front/src/app/novel/[slug]/page.tsx
+++ front/src/app/novel/[slug]/page.tsx
@@ -1,24 +1,16 @@
 import Card from '@/components/novel/Card';
+import CardList from '@/components/novel/CardList';
+import { Directory } from '@/model/workspace';
+import { getWorkspace } from '@/service/api/workspace';
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
 
 export default function NovelPage() {
+
   return (
     <div className="pl-48 font-melody">
-      <div className="font-extrabold text-4xl my-32">퇴근후에 만나요</div>
-      <div className="flex flex-col gap-20">
-        {[0, 1, 2].map((el, i) => (
-          <div key={i} className="flex flex-col gap-4">
-            <Card subject={`${i + 1}권`} />
-            <div className="flex flex-wrap gap-6">
-              {[0, 1, 2, 3, 4].map((elem, index) => (
-                <Link key={index} href="/editor">
-                  <Card subject={`${index + 1}화`} />
-                </Link>
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
+      <CardList />
     </div>
   );
 }
