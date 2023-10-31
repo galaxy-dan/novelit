e2d9c356beacb37de03f1de9e3446a3496467c89
JungInLee0130 <dlwjddls0130@naver.com>
2023-10-31 14:26:12 +0900

    Feat : Plot crud 완성

--- server/src/main/java/com/galaxy/novelit/plot/repository/PlotRepository.java
+++ server/src/main/java/com/galaxy/novelit/plot/repository/PlotRepository.java
@@ -1,11 +1,15 @@
 package com.galaxy.novelit.plot.repository;
 
 import com.galaxy.novelit.plot.entity.PlotEntity;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlotRepository extends JpaRepository <PlotEntity, Long> {
+
+    Optional<List<PlotEntity>> findAllByWorkspaceUuid(String workspaceUuid);
     Optional<PlotEntity> findPlotEntityByPlotUuid(String plotUuid);
+    Optional<PlotEntity> deletePlotEntitiesByPlotUuid(String plotUuid);
 }
