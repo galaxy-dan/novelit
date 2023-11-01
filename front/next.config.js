89dce41060952bb95624a69b4c31557eab1ff8c8
ssh102525 <ssh102525@naver.com>
2023-11-01 17:54:37 +0900

    chore: aws s3 이미지 링크 설정 추가

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'images.unsplash.com'],
+    domains: ['localhost', 'images.unsplash.com','novelit.s3.ap-northeast-2.amazonaws.com'],
   }
 }
 
