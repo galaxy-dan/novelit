388be94061ff7b51c7bf0f09dbddb3f909c60dc9
JeonJungyu <wjswnsrb93@naver.com>
2023-11-02 14:01:13 +0900

    chore: image 도메인 추가

--- front/next.config.js
+++ front/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'images.unsplash.com'],
-  }
-}
+    domains: ['localhost', 'images.unsplash.com', 'i.ibb.co'],
+  },
+};
 
 module.exports = nextConfig;
