ba9c4bbfd7f51ee533c56f5f12dbb31b86c432f1
chemwang13 <chemwang13@naver.com>
2023-11-06 16:04:10 +0900

    feat: words Create, update, delete feature added

--- server/src/main/java/com/galaxy/novelit/words/repository/WordsRepository.java
+++ server/src/main/java/com/galaxy/novelit/words/repository/WordsRepository.java
@@ -1,10 +1,13 @@
 package com.galaxy.novelit.words.repository;
 
 import com.galaxy.novelit.words.entity.WordsEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WordsRepository extends JpaRepository<WordsEntity,Long> {
-    WordsEntity findByWordUuid(String wordUuid);
+    Optional<WordsEntity> findByWordUuid(String wordUuid);
+
+    void deleteByWordUuid(String wordUuid);
 }
