2a044929e49fa0a1990913d25b05d0a8836cfa0e
jhp336 <jhp336@naver.com>
2023-11-07 13:00:23 +0900

    [S09P31A602-399] feat: 토큰 체크 위한 DTO 추가

---
+++ server/src/main/java/com/galaxy/novelit/share/dto/request/ShareFileReqDTO.java
@@ -0,0 +1,12 @@
+package com.galaxy.novelit.share.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ShareFileReqDTO {
+	private String token;
+}
