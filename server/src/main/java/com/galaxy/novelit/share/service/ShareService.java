c88d3452a9c4f0b63c9123d5cc593cece573c581
jhp336 <jhp336@naver.com>
2023-11-09 15:31:01 +0900

    feat: 공유 토큰 체크 api 추가

--- server/src/main/java/com/galaxy/novelit/share/service/ShareService.java
+++ server/src/main/java/com/galaxy/novelit/share/service/ShareService.java
@@ -2,9 +2,11 @@ package com.galaxy.novelit.share.service;
 
 import com.galaxy.novelit.share.dto.request.EditableReqDTO;
 import com.galaxy.novelit.share.dto.response.ShareTokenResDTO;
+import com.galaxy.novelit.share.dto.response.ShareTokenValidationResDTO;
 
 public interface ShareService {
 
     ShareTokenResDTO generateToken(String directoryUUID, String userUUID);
     void updateEditable(EditableReqDTO dto, String userUUID);
+    ShareTokenValidationResDTO validateToken(String token);
 }
