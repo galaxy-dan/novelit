9292549367c2bcc7e9f21d3a7732cc18d6dbe51b
JeonJungyu <wjswnsrb93@naver.com>
2023-11-10 15:00:19 +0900

    feat: sse 기능 추가

--- front/src/components/SSE.tsx
+++ front/src/components/SSE.tsx
@@ -16,17 +16,6 @@ export default function SSE() {
   const queryClient = useQueryClient();
 
   
- 
-  const CloseButton = ({ closeToast }: any, link: string) => (
-    <i
-      className="material-icons"
-      onClick={() => {
-        closeToast();
-      }}
-    >
-      이동
-    </i>
-  );
 
   useEffect(() => {
     const accessToken = localStorage.getItem('accessToken');
@@ -49,51 +38,7 @@ export default function SSE() {
     );
 
     eventSource.addEventListener('alertComment', function (event: any) {
-      console.log(event);
-
-      const data: Alarm = JSON.parse(event.data);
-      // const data = event.data;
-      // console.log(event);
-
-      if (event.type !== 'connection') {
-        const messageData = getAlarmMessage(data);
-        toast.info(messageData.message, {
-          autoClose: false,
-          // closeButton: (data) => CloseButton(data, messageData.link),
-        });
-        queryClient.invalidateQueries({ queryKey: ['alarm'] });
-      }
-      // (async () => {
-      //   // 브라우저 알림
-      //   const showNotification = () => {
-      //     const notification = new Notification('코드 봐줘', {
-      //       body: data.content,
-      //     });
-
-      //     setTimeout(() => {
-      //       notification.close();
-      //     }, 10 * 1000);
-
-      //     notification.addEventListener('click', () => {
-      //       window.open(data.url, '_blank');
-      //     });
-      //   };
-
-      //   // 브라우저 알림 허용 권한
-      //   let granted = false;
-
-      //   if (Notification.permission === 'granted') {
-      //     granted = true;
-      //   } else if (Notification.permission !== 'denied') {
-      //     let permission = await Notification.requestPermission();
-      //     granted = permission === 'granted';
-      //   }
-
-      //   // 알림 보여주기
-      //   if (granted) {
-      //     showNotification();
-      //   }
-      // })();
+      toast(event.data);
     });
     eventSource.onmessage = (error) => {
       console.log(error);
