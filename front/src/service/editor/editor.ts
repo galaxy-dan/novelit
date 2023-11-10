16dd5b4862a77a06854460b392882a26ac892c29
JeonJungyu <wjswnsrb93@naver.com>
2023-11-10 15:05:00 +0900

    feat: 에디터 jwt 함수 추가

--- front/src/service/editor/editor.ts
+++ front/src/service/editor/editor.ts
@@ -1,2 +1,16 @@
 export const fontSize = ['lg', 'xl', '2xl', '3xl', '4xl'];
-export const fontFamily = ['melody', 'nanum', 'noto', 'nanumPen'];
+export const fontFamily = [
+  'melody',
+  'nanum',
+  'noto',
+  'nanumPen',
+  'jalnan',
+  'jalnanGothic',
+];
+
+export const getJwtPayload = (token: string) => {
+  const base64Payload = token.split('.')[1];
+  const payload = Buffer.from(base64Payload, 'base64');
+  const result = JSON.parse(payload.toString());
+  return result;
+};
