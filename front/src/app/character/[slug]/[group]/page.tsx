8cd15f89053ec0bce2d2d4e5cf7422cae2ede5d8
ssh102525 <ssh102525@naver.com>
2023-11-15 11:07:13 +0900

    feat: 캐릭터 화면 캐릭터 및 그룹 추가 버튼 구현

--- front/src/app/character/[slug]/[group]/page.tsx
+++ front/src/app/character/[slug]/[group]/page.tsx
@@ -6,14 +6,18 @@ import CharacterUpperGroup from '@/components/character/CharacterUpperGroup';
 import GroupCardGroup from '@/components/character/GroupCardGroup';
 import GroupName from '@/components/character/GroupName';
 import { groupItemType } from '@/model/charactor';
-import { getSubGroupAndCharacter } from '@/service/api/group';
+import { postCharacter } from '@/service/api/character';
+import { getSubGroupAndCharacter, postGroup } from '@/service/api/group';
 import {
   UseQueryResult,
+  useMutation,
   useQuery,
   useQueryClient,
 } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
+import { MdGroupAdd, MdPersonAdd } from 'react-icons/md';
+import { v4 as uuidv4 } from 'uuid';
 
 type Props = {
   params: {
@@ -39,6 +43,22 @@ export default function page({ params }: Props) {
     staleTime: 0,
   });
 
+  const postCharacterMutate = useMutation({
+    mutationFn: postCharacter,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['group']);
+      queryClient.invalidateQueries(['characterDirectory']);
+    },
+  });
+
+  const postGroupMutate = useMutation({
+    mutationFn: postGroup,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['group']);
+      queryClient.invalidateQueries(['characterDirectory']);
+    },
+  });
+
   const [groupNameInput, setGroupNameInput] = useState<string>('');
 
   return (
@@ -50,7 +70,39 @@ export default function page({ params }: Props) {
       />
       <CharacterHomeTitle slug={params.slug} />
       {/* 검색 창 */}
-      <CharacterNameSearch slug={params.slug} />
+      <div className="flex justify-between w-full">
+        <CharacterNameSearch slug={params.slug} />
+        <div className="flex mt-11 mr-[10rem]">
+          <div
+            onClick={() => {
+              const uuid = uuidv4();
+              postGroupMutate.mutate({
+                workspaceUUID: params.slug,
+                groupName: '새 그룹',
+                parentGroupUUID: params.group,
+                groupUUID: uuid,
+              });
+            }}
+            className="flex items-center bg-neutral-200 hover:bg-neutral-400 rounded-md w-fit px-2 py-1 text-2xl cursor-pointer font-bold mr-2"
+          >
+            <MdGroupAdd className="mr-2" />새 그룹
+          </div>
+          <div
+            onClick={() => {
+              const uuid = uuidv4();
+              postCharacterMutate.mutate({
+                workspace: params.slug,
+                group: params.group,
+                name: '새 캐릭터',
+                uuid: uuid,
+              });
+            }}
+            className="flex items-center bg-neutral-200 hover:bg-neutral-400 rounded-md w-fit px-2 py-1 text-2xl cursor-pointer font-bold"
+          >
+            <MdPersonAdd className="mr-2" />새 캐릭터
+          </div>
+        </div>
+      </div>
       {/* 캐릭터 카드 전체 모음 */}
       <div className="mt-6">
         {/* 캐릭터 카드 그룹 */}
